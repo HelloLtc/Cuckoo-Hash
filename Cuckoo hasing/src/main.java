291c7d990cf25a6414331775d733ecdcab14865a
HelloLtc <39508323+HelloLtc@users.noreply.github.com>
2021-05-10 14:14:09 +0800

    Update main.java

--- Cuckoo hasing/src/main.java
+++ Cuckoo hasing/src/main.java
@@ -6,15 +6,21 @@ import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
+
+        //Initialize a database
         int data_size = 10;
         int ELEMENT_SIZE= (int) Math.pow(2, data_size);//size of all elements
         int DEFAULT_INITIAL_CAPACITY = (int) Math.floor((ELEMENT_SIZE * (1 + 0.3)));//size of each table.(two table store all elements)
-        int keyword = 722;//search keyword
+        long[] table = new long[ELEMENT_SIZE];
+        for (int i = 0; i < ELEMENT_SIZE; i++) {
+            table[i]=i;
+        }
 
 
+        //Construct a cuckoo hash table
         cuckoo cuckoo = new cuckoo();
-        long[] cuckoo_EMM = cuckoo.setup(data_size);//initialization
-        ArrayList<Integer> cuckoo_Stash = cuckoo.Get_Cuckoo_Stash();//
+        long[] cuckoo_EMM = cuckoo.setup(table,data_size);
+        ArrayList<Integer> cuckoo_Stash = cuckoo.Get_Cuckoo_Stash();
         for(int i=0;i<cuckoo_Stash.size();i++){
             System.out.println("Stash:"+cuckoo_Stash.get(i));
         }
@@ -22,6 +28,7 @@ public class main {
 
 
         //Query
+        int keyword = 984;//search keyword
         int t_0 =  Hash.reduce((int) Hash.hash64(keyword,0),DEFAULT_INITIAL_CAPACITY);//first possible position
         int t_1 =  Hash.reduce((int) Hash.hash64(keyword,1),DEFAULT_INITIAL_CAPACITY)+DEFAULT_INITIAL_CAPACITY;//second possible position
         String res = cuckoo_EMM[t_0]+" "+cuckoo_EMM[t_1];
